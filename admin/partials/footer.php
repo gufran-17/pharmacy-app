6ba7fba2b7f624f5e1fa259de63a97a8156ae025
gufarn-17 <gufarnansari1717@gamial.com>
2026-04-17 10:57:51 +0530

    fixed footer

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -4,33 +4,38 @@
     <!-- Grid container starts -->
     <div class="container p-4 pb-0">
       <!-- Section: Social media starts -->
-      <section class="mb-4">
-        <!-- Facebook -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+          <section class="mb-4">
 
-        <!-- Twitter -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+  <!-- Instagram -->
+  <a class="btn btn-outline-light btn-floating m-1" 
+     href="https://www.instagram.com/gufran_1718/" 
+     target="_blank" role="button">
+    <i class="fab fa-instagram"></i>
+  </a>
 
-        <!-- Google -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
+  <!-- Linkedin -->
+  <a class="btn btn-outline-light btn-floating m-1" 
+     href="https://www.linkedin.com/in/gufran-ansari-11331b373" 
+     target="_blank" role="button">
+    <i class="fab fa-linkedin-in"></i>
+  </a>
 
-        <!-- Instagram -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+  <!-- Github -->
+  <a class="btn btn-outline-light btn-floating m-1" 
+     href="https://github.com/gufran-17" 
+     target="_blank" role="button">
+    <i class="fab fa-github"></i>
+  </a>
 
-        <!-- Linkedin -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
-
-        <!-- Github -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-github"></i></a>
-      </section>
+</section>
       <!-- Section: Social media ends-->
     </div>
     <!-- Grid container ends-->
 
     <!-- Copyright starts -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-      © 2021 Copyright:
-      <a class="text-white" href="#">Bhumika Joshi</a>
+      © 2026 Copyright:
+      <a class="text-white" href="#">Gufran Ansari</a>
     </div>
     <!-- Copyright ends -->
   </footer>
