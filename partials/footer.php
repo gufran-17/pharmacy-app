6eaf3209100e9b6454c3c7d9a2b22c50b45f1f0e
gufarn-17 <gufarnansari1717@gamial.com>
2026-04-17 10:30:04 +0530

    fixed footer

--- partials/footer.php
+++ partials/footer.php
@@ -8,24 +8,29 @@
   <div class="container p-4 pb-0">
     <!-- Section: Social media starts -->
     <section class="mb-4">
-      <!-- Facebook -->
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
 
-      <!-- Twitter -->
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+  <!-- Instagram -->
+  <a class="btn btn-outline-light btn-floating m-1" 
+     href="https://www.instagram.com/gufran_1718/" 
+     target="_blank" role="button">
+    <i class="fab fa-instagram"></i>
+  </a>
 
-      <!-- Google -->
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-google"></i></a>
+  <!-- Linkedin -->
+  <a class="btn btn-outline-light btn-floating m-1" 
+     href="https://www.linkedin.com/in/gufran-ansari-11331b373" 
+     target="_blank" role="button">
+    <i class="fab fa-linkedin-in"></i>
+  </a>
 
-      <!-- Instagram -->
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+  <!-- Github -->
+  <a class="btn btn-outline-light btn-floating m-1" 
+     href="https://github.com/gufran-17" 
+     target="_blank" role="button">
+    <i class="fab fa-github"></i>
+  </a>
 
-      <!-- Linkedin -->
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
-
-      <!-- Github -->
-      <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-github"></i></a>
-    </section>
+</section>
     <!-- Section: Social media ends-->
   </div>
   <!-- Grid container ends-->
